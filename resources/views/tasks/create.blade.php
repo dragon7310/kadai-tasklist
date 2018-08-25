d73a03c9b5b9af5cfd62fff9a75b30a9620bb9a5
dragon7310 <y_hollys@yahoo.co.jp>
2018-08-25 09:34:39 +0000

    validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,11 +4,13 @@
 
 <h1>タスク新規作成ページ</h1>
 
+
 {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
 {!! Form::label('content', 'タスク：') !!}
 {!! Form::text('content') !!}
 {!! Form::submit('投稿') !!}
 
+{!! Form::close() !!}
 
 @endsection
